4c67c2d1e7f73f73e16729037499fbadc89c6ded
Janderson Martins <jandersonmartins10@gmail.com>
2025-01-09 22:35:23 -0300

    nerdtree show hidden files

--- lua/plugins/nerdtree.lua
+++ lua/plugins/nerdtree.lua
@@ -3,6 +3,7 @@ return {
     config = function()
         -- global configs
         vim.g.NERDTreeWinPos = "right"
+        vim.g.NERDTreeShowHidden = 1
         -- keymaps
         vim.keymap.set("n", "<C-a>", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
     end
