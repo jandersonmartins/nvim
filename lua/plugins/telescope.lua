b4da82f7fa7c8fa938e33171ff690078ef515645
Janderson Martins <jandersonmartins10@gmail.com>
2025-01-09 23:04:25 -0300

    telescope ignoring directories

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,15 @@ return {
     "nvim-telescope/telescope.nvim", tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        require("telescope").setup({})
+        require("telescope").setup({
+            defaults = {
+                file_ignore_patterns = { 
+                    "node_modules/",
+                    "app/build/",
+                    "build/"
+                }
+            }
+        })
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
